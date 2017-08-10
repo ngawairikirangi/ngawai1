a8d903770882ef57b9d866a5179e0d94dd662d26
Ngawai Rikirangi <9978212@BCS.NET.NZ>
2017-08-10 17:01:40 +1200

    completed task 1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            var fname = Console.ReadLine();
+            Console.WriteLine(fname);
+
+            Console.WriteLine("What is your last name?");
+            var lname = Console.ReadLine();
+            Console.WriteLine(lname);
+
+        }
+    }
+}
